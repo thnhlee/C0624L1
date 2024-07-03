b841a1c112313cb7a88f26040d19fade3c3901e8
thinhnee <lht1823k@gmail.com>
2024-07-03 20:58:29 +0700

    Array

---
+++ Array/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                
+            /////////////////////////////////////Nhập 10 số nguyên cho mảng
+            double[] numberList = new double[10];
+            Console.WriteLine("Nhap diem:");
+
+            for (int i = 0 ; i < numberList.Length; i++)
+            {
+                Console.Write($"Diem thu {i + 1}: ");
+                numberList[i] = double.Parse(Console.ReadLine());
+            }
+
+
+            Console.WriteLine("Toan bo diem:");
+            for(int i = 0; i < numberList.Length; i++)
+            {
+                Console.Write($"{numberList[i]}; ");
+            }
+
+            // double DiemLonNhat = numberList.Max();
+            // double DiemNhoNhat = numberList.Min();
+            double diemLonNhat = numberList[0];
+            double diemNhoNhat = numberList[0];
+
+            for(int y = 1; y < numberList.Length; y++)
+            {
+                if(numberList[y] > diemLonNhat)
+                {
+                    diemLonNhat = numberList[y];
+                }
+                
+                if(numberList[y] < diemNhoNhat)
+                {
+                    diemNhoNhat = numberList[y];
+                }
+            }
+
+            Console.WriteLine($"\nDiem lon nhat: {diemLonNhat}");
+            Console.WriteLine($"Diem nho nhat: {diemNhoNhat}");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
